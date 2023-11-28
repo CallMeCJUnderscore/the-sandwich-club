ba0f2f06845230e8c8e5380456a5cded0c037307
amjadalsaidi <146876370+amjadalsaidi@users.noreply.github.com>
2023-11-28 14:58:17 -0500

    Updated the type of the size attribute in the Drink class, Updated the code for the getPrice method

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -3,23 +3,23 @@ package com.pluralsight;
 public class Drink implements Product{
     /*---------------VARIABLES---------------*/
 
-    public char size;
+    public String size;
     public String flavor;
 
     /*--------------CONSTRUCTORS-------------*/
 
-    public Drink(char size, String flavor) {
+    public Drink(String size, String flavor) {
         this.size = size;
         this.flavor = flavor;
     }
 
     /*------------GETTERS/SETTERS------------*/
 
-    public char getSize() {
+    public String getSize() {
         return size;
     }
 
-    public void setSize(char size) {
+    public void setSize(String size) {
         this.size = size;
     }
 
@@ -35,6 +35,28 @@ public class Drink implements Product{
 
     @Override
     public double getPrice() {
-        return 0;
+        // Define base prices for each size
+        double smallPrice = 2.0;
+        double mediumPrice = 2.5;
+        double largePrice = 3.0;
+
+        // Assign the price based on the size of the drink
+        double price = 0.0;
+        switch (size.toUpperCase()) {
+            case "S":
+                price = smallPrice;
+                break;
+            case "M":
+                price = mediumPrice;
+                break;
+            case "L":
+                price = largePrice;
+                break;
+            default:
+                System.out.println("Invalid size");
+        }
+
+
+        return price;
     }
 }
