cacc27c53df7859e6dce598dcbd2150f9b039854
codenamehannah <jhannahstuart@gmail.com>
2023-11-28 18:05:38 -0500

    Adjusted the constructor in the order class.

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,5 +1,6 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
@@ -12,49 +13,10 @@ public class Order {
     /*--------------CONSTRUCTORS-------------*/
 
     public Order(List<Sandwich> sandwiches, List<Drink> drinks, List<Chips> chips) {
-        this.sandwiches = sandwiches;
-        this.drinks = drinks;
-        this.chips = chips;
+        this.sandwiches = new ArrayList<>();
+        this.drinks = new ArrayList<>();
+        this.chips = new ArrayList<>();
+
     }
-
-    /*------------GETTERS/SETTERS------------*/
-
-    public List<Sandwich> getSandwiches() {
-        return sandwiches;
-    }
-
-    public void setSandwiches(List<Sandwich> sandwiches) {
-        this.sandwiches = sandwiches;
-    }
-
-    public List<Drink> getDrinks() {
-        return drinks;
-    }
-
-    public void setDrinks(List<Drink> drinks) {
-        this.drinks = drinks;
-    }
-
-    public List<Chips> getChips() {
-        return chips;
-    }
-
-    public void setChips(List<Chips> chips) {
-        this.chips = chips;
-    }
-
-    /*---------------FUNCTIONS---------------*/
-
-    public Sandwich orderSandwich(){
-
-        return null;
-    }
-
-    public Drink orderDrink(){
-        return null;
-    }
-
-    public Chips orderChips(){
-        return null;
-    }
+    
 }
