4a20ef4f96c3952a26fd5560e107fcede760e05f
CallMeCJ_ <36345590+CallMeCJUnderscore@users.noreply.github.com>
2023-11-30 14:44:36 -0500

    Reverted to previous version + Implemented pricing method for all classes (#12)

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -97,18 +97,12 @@ public class Drink implements Product{
 
         // assign the price based on the size of the drink
         double price = 0.0;
-        switch (this.size) {
-            case "S":
-                price = smallPrice;
-                break;
-            case "M":
-                price = mediumPrice;
-                break;
-            case "L":
-                price = largePrice;
-                break;
-            default:
-                System.out.println("Invalid size");
+
+        switch (size) {
+            case 'S' -> price = smallPrice;
+            case 'M' -> price = mediumPrice;
+            case 'L' -> price = largePrice;
+            default -> System.out.println("Invalid size");
         }
         return price;
     }
