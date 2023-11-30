cb0cbda3e88f1b526496e36d0fb4ce9bbb138de5
Jaarona Hannah Stuart <139713605+codenamehannah@users.noreply.github.com>
2023-11-30 11:38:29 -0500

    Feature neworder menu (#11)
    * Updated the Program Class with the Main Menu with two options.
    
    * Committing to ensure that I saved all of my minor changes to the order menu

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -11,62 +11,84 @@ public class Program {
     public Program() {
     }
     public static void main(String[] args) {
-
+        displayMainMenu();
+        displayMenu();
     }
-
-    public static void displayMenu() {
-        System.out.println("The Sandwich Club Order Menu:");
-        System.out.println("1. Sandwich");
-        System.out.println("2. Drink");
-        System.out.println("3. Chips");
-        System.out.println("4. Checkout");
-        System.out.println("5. Quit");
-        System.out.println("6. Go Back"); // Option to go back
-        System.out.print("Select an option: ");
-
-
+    //Main Menu
+    public static void displayMainMenu() {
+        System.out.println("Press 1 for New Order");
+        System.out.println("Press 2 for quit");
         Scanner scanner = new Scanner(System.in);
-        boolean quit = false;
-        ReceiptManager receiptManager = new ReceiptManager();
+        boolean condition = true;
+        while (condition == true) {
+            int option = scanner.nextInt();
+            if (option == 1) {
+                displayMenu();
+                condition = false;
+            } else if (option == 2) {
+                System.out.println("Quiting now, Thank you!");
+                condition = false;
+            } else {
+                System.out.println("Invalid option");
 
-        while (!quit) {
-            int choice = scanner.nextInt();
-            scanner.nextLine(); //  newline character
-            Product var4;
+                //Order Menu
+                public static void displayMenu () {
+                    System.out.println("The Sandwich Club Order Menu:");
 
-            switch (choice) {
-                case 1:
-                    var4 = Sandwich.order();
-                    break;
-                case 2:
-                    var4 = Drink.order();
-                    break;
-                case 3:
-                    var4 = Chips.order();
-                    break;
-                case 4:
-                    receiptManager.saveReceipt(Sandwich.order, Sandwich.order.getPrice());                    break;
-                case 5:
-                    quit = true;
-                    System.out.println("Program terminated.");
-                    break;
-                case 6:
-                    goBack();
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please select again.");
-                    break;
+                    System.out.println("1. Sandwich");
+                    System.out.println("2. Drink");
+                    System.out.println("3. Chips");
+                    System.out.println("4. Checkout");
+                    System.out.println("5. Quit");
+                    System.out.println("6. Go Back"); // Option to go back
+                    System.out.print("Select an option: ");
+
+
+                    Scanner scanner = new Scanner(System.in);
+                    boolean quit = false;
+                    ;
+
+                    Order order = new Order();
+                    ReceiptManager receiptManager = new ReceiptManager()
+                    while (!quit) {
+                        int choice = scanner.nextInt();
+                        scanner.nextLine(); //  newline character
+                        Product item;
+
+                        switch (choice) {
+                            case 1:
+                                order = new Order();
+                                break;
+                            case 2:
+                                item = Drink.order();
+                                break;
+                            case 3:
+                                item = Chips.order();
+                                break;
+                            case 4:
+                                receiptManager.saveReceipt(order, order.getPrice());
+                                break;
+                            case 5:
+                                quit = true;
+                                System.out.println("Program terminated.");
+                                break;
+                            case 6:
+                                System.out.println("I am outside");
+                                displayMainMenu();
+                                return;
+
+                            default:
+                                System.out.println("Invalid choice. Please select again.");
+                                break;
+                        }
+                    }
+                    scanner.close();
+                }
+
+
+                private static void goBack () {
+                    System.out.println("Going back to the previous menu...");
+
+                }
             }
-        }
-        scanner.close();
-    }
 
-
-
-
-
-    private static void goBack() {
-        System.out.println("Going back to the previous menu...");
-
-    }
-}
